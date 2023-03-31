8236e275a1975a81b2f88bb7647a06da2e7ad4e8
Ryan Chen <ryanchen.ylc@gmail.com>
2023-03-31 15:18:00 +0800

    update multiple_robots localization files & rviz.

---
+++ src/turtlebot3/turtlebot3_slam/config/multiple_robots/cartographer_localization_first.lua
@@ -0,0 +1,15 @@
+include "cartographer_mapping_first.lua"
+
+TRAJECTORY_BUILDER.pure_localization_trimmer = {
+  max_submaps_to_keep = 2,
+}
+POSE_GRAPH.optimize_every_n_nodes = 3
+
+MAP_BUILDER.num_background_threads = 8
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.5 * POSE_GRAPH.constraint_builder.sampling_ratio
+POSE_GRAPH.global_sampling_ratio = 0.1 * POSE_GRAPH.global_sampling_ratio
+POSE_GRAPH.max_num_final_iterations = 1
+
+
+
+return options
